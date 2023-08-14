aea41f6a09e3feded2b7dbf708102e95ff39472c
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-14 19:21:43 +0000

    create App component

---
+++ src/js/component/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Player from './js/component/Player';
+import './styles/index.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Reproductor "Estilo Spotify"</h1>
+      <Player />
+    </div>
+  );
+};
+
+export default App;
