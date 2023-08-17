db08e94290c0936ac8febfb1ae8e6fd9735b9b76
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-17 22:15:45 +0000

    functionality of the player finished

--- src/js/component/Player.jsx
+++ src/js/component/Player.jsx
@@ -1,30 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
-import { FaStepBackward, FaPlay, FaStepForward } from 'react-icons/fa';
+import { FaStepBackward, FaPlay, FaPause, FaStepForward } from 'react-icons/fa';
 
 const Player = () => {
+  const [songs, setSongs] = useState([]);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    fetch('https://playground.4geeks.com/apis/fake/sound/songs')
+      .then(response => response.json())
+      .then(data => {
+        setSongs(data);
+      })
+      .catch(error => {
+        console.error('Error fetching songs:', error);
+      });
+  }, []);
+
+  const playSong = (songUrl, index) => {
+    if (audioRef.current) {
+      audioRef.current.src = `https://playground.4geeks.com/apis/fake/sound/${songUrl}`;
+      audioRef.current.play();
+      setCurrentSongIndex(index);
+      setIsPlaying(true);
+    }
+  };
+
+  const togglePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const playNextSong = () => {
+    let nextIndex = (currentSongIndex + 1) % songs.length;
+    playSong(songs[nextIndex].url, nextIndex);
+  };
+
+  const playPreviousSong = () => {
+    let previousIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+    playSong(songs[previousIndex].url, previousIndex);
+  };
+
   return (
-    <div className="player-container bg-dark text-white p-3">
-      <div className="songs-list text-center mb-4">
-        <h2 className="mb-5">Lista de Canciones</h2>
+    <div className="player-container">
+      <div className="songs-list">
+        <h2>Lista de Canciones</h2>
         <ListGroup>
-          <ListGroup.Item className="bg-dark border-0 text-white">Nombre canción 1</ListGroup.Item>
-          <div className="divider bg-secondary my-2"></div>
-          <ListGroup.Item className="bg-dark border-0 text-white">Nombre canción 2</ListGroup.Item>
-          <div className="divider bg-secondary my-2"></div>
-          {/* Agregar más elementos de la lista aquí */}
+          {songs.map((song, index) => (
+            <ListGroup.Item
+              key={index}
+              className="bg-dark border-0 text-white"
+              onClick={() => playSong(song.url, index)}
+            >
+              {song.name}
+            </ListGroup.Item>
+          ))}
         </ListGroup>
       </div>
-      <div className="divider bg-secondary mb-4"></div>
-      <div className="controls fixed-bottom d-flex justify-content-center p-3 bg-dark">
-        <Button variant="light" className="control-button">
-          <FaStepBackward /> {/* Icono de retroceder */}
+      <audio ref={audioRef} />
+
+      <div className="controls">
+        <Button variant="light" className="control-button" onClick={playPreviousSong}>
+          <FaStepBackward />
         </Button>
-        <Button variant="light" className="control-button mx-3">
-          <FaPlay /> {/* Icono de play */}
+        <Button variant="light" className="control-button" onClick={togglePlayPause}>
+          {isPlaying ? <FaPause /> : <FaPlay />}
         </Button>
-        <Button variant="light" className="control-button">
-          <FaStepForward /> {/* Icono de avanzar */}
+        <Button variant="light" className="control-button" onClick={playNextSong}>
+          <FaStepForward />
         </Button>
       </div>
     </div>
